5f4d4a94a3395f6dded1613081838d6357922305
younusaliakash <younusalikash8@gmail.com>
2023-12-21 23:43:40 +0600

    (feat): add Column, delete column function and UI done

---
+++ src/components/Board.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon";
+import { Column, Id } from "../types";
+import ColumnContainer from "./ColumnContainer";
+
+const Board = () => {
+  const [columns, setColumn] = useState<Column[]>([]);
+
+  console.log(columns);
+
+  function createNewColumn() {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+
+    setColumn([...columns, columnToAdd]);
+  }
+
+  function generateId() {
+    return Math.floor(Math.random() * 10001);
+  }
+
+  function deleteColumn(id: Id) {
+    const filterColumn = columns.filter((column) => column.id !== id);
+    setColumn(filterColumn);
+  }
+
+  return (
+    <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+      <div className="m-auto flex gap-4">
+        <div className="flex gap-4">
+          {columns?.map((column, idx) => (
+            <ColumnContainer
+              deleteColumn={deleteColumn}
+              column={column}
+              key={idx}
+            />
+          ))}
+        </div>
+        <button
+          onClick={createNewColumn}
+          className="h-[60px] w-[250px] min-w-[250px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 p-4 ring-rose-500 hover:ring-2 flex gap-2"
+        >
+          <PlusIcon />
+          Add Column
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
